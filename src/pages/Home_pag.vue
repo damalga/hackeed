420df9fefc2c305f59085ace76ccb10376dcd17a
damalga <damalga@protonmail.com>
2025-09-07 20:01:59 +0200

    style updates

--- src/pages/Home_pag.vue
+++ src/pages/Home_pag.vue
@@ -1,20 +1,18 @@
 <template>
-    <div>
-        <Header />
-        <Hero />
-        <Products
-            title="Productos destacados"
-            :limit="3"
-            :show-title="true"
-        />
-        <Footer />
-    </div>
+  <div>
+    <Header />
+    <main class="home-main">
+      <Hero />
+      <Products title="Productos destacados" :limit="3" :show-title="true" />
+    </main>
+    <Footer />
+  </div>
 </template>
 
 <script setup>
-import Header from "../components/Header_comp.vue";
-import Hero from "../components/Hero_comp.vue";
-import Products from "../components/Products_comp.vue";
-import Contact from "../components/Contact_comp.vue";
-import Footer from "../components/Footer_comp.vue";
+import Header from '../components/Header_comp.vue'
+import Hero from '../components/Hero_comp.vue'
+import Products from '../components/Products_comp.vue'
+import Contact from '../components/Contact_comp.vue'
+import Footer from '../components/Footer_comp.vue'
 </script>
