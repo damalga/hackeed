225197b2ee6e7ebaf388f0f392c330fe7831b1c5
damalga <damalga@protonmail.com>
2025-10-01 18:02:25 +0200

    fix footer conflit

--- src/components/Footer_comp.vue
+++ src/components/Footer_comp.vue
@@ -9,13 +9,8 @@
       </div>
       <div class="footer-claim">
         <span class="footer-claim">
-<<<<<<< HEAD
-          Este sitio usa cookies técnicas necesarias para procesar pagos (Stripe).
-          <br />Para más información en lo que respecta al uso de tus datos por favor visita:
-=======
           Este sitio usa cookies técnicas necesarias para procesar pagos (Stripe). Para más
           información en lo que respecta al uso de tus datos por favor visita:
->>>>>>> feat/stripe
           <router-link to="/sfaq" class="footer-link">SFAQ</router-link>
         </span>
         <br />
